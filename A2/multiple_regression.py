e416077459c98e9de192512673779560d7aef64a
spyder21er <spyder21er@gmail.com>
2019-09-21 00:38:02 +0800

    Updated Assignment #2 folder.

--- A2/multiple_regression.py
+++ A2/multiple_regression.py
@@ -33,15 +33,29 @@ def derivative(theta, data, j):
         sum += (hypothesis(theta, data[x, :-1]) - data[x, last]) * data[x, j]
     return (sum / (2.0 * m))[0,0]
     
+def saveThetaToFile(filename, iteration_number, cost, theta):
+    f = open(filename, "a+")
+    f.write("================================================================\n")
+    f.write("Iteration %d: \n" % iteration_number)
+    f.write("Cost: %s\n" % cost)
+    f.write("Theta: ")
+    for x in theta:
+        f.write("%s, " % x)
+    f.write("\n")
+    f.close
+    
 data = load("housing.csv")
 m, n = data.shape
-theta = [0.0] * (n-1)
+hundreds = 53998
+#5399800 iterations
+theta = [0.8153050407908885, -0.24023534058602747, 0.0471576106975837, 0.009900074436971064, 1.1710530133311983, 0.20420348704975436, 5.732643253834673, -0.006477385094260471, -0.9134626478799869, 0.19470673024282908, -0.010721052501982416, -0.44242716785141745, 0.015811638826258117, -0.46158425458534974]
 new_theta = [0.0] * (n-1)
 df = [0.0] * (n-1)
 alpha = 0.1
-limit = 1e-11
+limit = 1e-15
 delta = 0.001
 iterations = 0
+outputfile = "return2.txt"
 
 while delta >= limit:
     iterations += 1
@@ -66,8 +80,15 @@ while delta >= limit:
         
     for x in range(0, n - 1):
         theta[x] = new_theta[x]
+    
+    # saveThetaToFile(outputfile, (hundreds*100+iterations), new, theta)
+        
+    if iterations == 100:
+        iterations = 0
+        hundreds += 1
+        saveThetaToFile(outputfile, hundreds*100, new, theta)
         
 print(theta)
-print(iterations)
-#3062 iterations
-# training started at 3:22 PM
+print(cost(theta, data))
+
+print(hundreds*100+iterations)
